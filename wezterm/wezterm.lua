4c0f0ba3e6029de607d8bcb7e41ad996b71ed4e1
keypeearr <kyle.requez.code@gmail.com>
2025-05-31 01:55:24 +0800

    Updated wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -135,8 +135,8 @@ for i = 0, 9 do
 end
 
 -- tab bar configuration
-config.hide_tab_bar_if_only_one_tab = false
-config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = true
 
